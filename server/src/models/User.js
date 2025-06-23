e8b1c9708f4dfed5e2022ca7b4a04e2d8f0e9fbc
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-23 07:13:34 -0300

    Add User model with Swagger documentation

---
+++ server/src/models/User.js
@@ -0,0 +1,131 @@
+import { Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcryptjs';
+import sequelize from '../config/database';
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *         - firstName
+ *         - lastName
+ *       properties:
+ *         id:
+ *           type: string
+ *           format: uuid
+ *           description: Unique identifier for the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: User's email address
+ *         password:
+ *           type: string
+ *           format: password
+ *           description: User's password (hashed)
+ *         firstName:
+ *           type: string
+ *           description: User's first name
+ *         lastName:
+ *           type: string
+ *           description: User's last name
+ *         profileImageUrl:
+ *           type: string
+ *           format: uri
+ *           description: URL to user's profile image
+ *         phone:
+ *           type: string
+ *           description: User's phone number
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: Timestamp when the user was created
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: Timestamp when the user was last updated
+ */
+class User extends Model {
+  async comparePassword(enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+  }
+  
+  // Remove sensitive fields when converting to JSON
+  toJSON() {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'Please provide a valid email address',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [6, 100],
+          msg: 'Password must be at least 6 characters long',
+        },
+      },
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    profileImageUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true,
+    hooks: {
+      // Hash password before saving
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+    },
+  }
+);
+
+export default User;
